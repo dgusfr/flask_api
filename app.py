63d652133071809751a7291858578fa1b73a4353
dgusfr <diegogustavo.fr@gmail.com>
2025-06-23 20:08:56 -0300

    add item creation functionality for stores

--- app.py
+++ app.py
@@ -27,3 +27,17 @@ def create_store():
     }
     store.append(new_store)
     return {"store": new_store}, 201
+
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for s in store:
+        if s["name"] == name:
+            new_item = {
+                "name": request_data["name"],
+                "price": request_data["price"],
+            }
+            s["items"].append(new_item)
+            return {"item": new_item}, 201
+    return {"message": "Store not found"}, 404
