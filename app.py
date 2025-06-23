5d08b8801e2564442b729eedbf16e962011510a8
dgusfr <diegogustavo.fr@gmail.com>
2025-06-23 18:31:08 -0300

    add initial Flask app setup with store data

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+store = [
+    {
+        "name": "My Store",
+        "items": [
+            {"name": "My Item", "price": 15.99},
+            {"name": "Another Item", "price": 9.99},
+        ],
+    }
+]
+
+
+@app.get("/store")
+def get_stores():
+    return {"stores": store}
