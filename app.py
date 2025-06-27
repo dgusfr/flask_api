73c55bb5d34ea250fa68068882f93d7c08aafd39
dgusfr <diegogustavo.fr@gmail.com>
2025-06-27 12:13:45 -0300

    refactor: move store and items definitions to db.py

--- app.py
+++ app.py
@@ -2,20 +2,6 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-store = {}
-items = {
-    1: {
-        "name": "My Item",
-        "price": 15.99,
-    },
-    2: {
-        "name": "Another Item",
-        "price": 9.99,
-    },
-}
-
-items[1]
-
 
 @app.get("/store")
 def get_stores():
---
+++ db.py
@@ -0,0 +1,13 @@
+store = {}
+items = {
+    1: {
+        "name": "My Item",
+        "price": 15.99,
+    },
+    2: {
+        "name": "Another Item",
+        "price": 9.99,
+    },
+}
+
+items[1]
