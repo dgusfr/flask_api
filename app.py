bbf6a10c67156aa5d2aa5cf1ce80933fd169219d
dgusfr <diegogustavo.fr@gmail.com>
2025-06-27 12:12:58 -0300

    fix: refactor store structure to use a dictionary instead of a list

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-store = [
-    {
-        "name": "My Store",
-        "items": [
-            {"name": "My Item", "price": 15.99},
-            {"name": "Another Item", "price": 9.99},
-        ],
-    }
-]
+store = {}
+items = {
+    1: {
+        "name": "My Item",
+        "price": 15.99,
+    },
+    2: {
+        "name": "Another Item",
+        "price": 9.99,
+    },
+}
+
+items[1]
 
 
 @app.get("/store")
