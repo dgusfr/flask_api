dd8b9bc67640cf092dfa2ddf696d346055b1335d
dgusfr <diegogustavo.fr@gmail.com>
2026-01-05 12:14:19 -0300

    feat(add): integrate MongoDB connection and load environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from .routes.main import main_bp
+from pymongo import MongoClient
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(main_bp)
+    app.config.from_object("config.Config")
+    global db
+
+    try:
+        client = MongoClient(app.config["MONGO_URI"])
+        db = client.get_default_database()
+    except Exception as e:
+        print("Erro na conexão de Banco de Dados.")
+
     return app
