ad27fed17cf0a719059cd9edecb3ce833b1a4d62
dgusfr <diegogustavo.fr@gmail.com>
2026-01-06 18:40:57 -0300

    feat(update)

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 
 class Config:
     MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY", "chave-padrao-dev-insegura")
