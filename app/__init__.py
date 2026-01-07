2787413d6db28481449c9e36698f0f47a7d8b2c7
dgusfr <diegogustavo.fr@gmail.com>
2026-01-07 12:34:14 -0300

    feat(add): implement authentication routes with login functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,31 @@
 from flask import Flask
 from pymongo import MongoClient
+import os
+
+
+db = None
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
+
     global db
 
     try:
         client = MongoClient(app.config["MONGO_URI"])
         db = client.get_default_database()
     except Exception as e:
-        print("Erro na conexão de Banco de Dados.")
+        print(f"Erro na conexão de Banco de Dados: {e}")
 
-    from .routes.main import main_bp
+    from .routes.auth import auth_bp
+    from .routes.products import products_bp
 
-    app.register_blueprint(main_bp)
+    from .routes.users import user_bp
+
+    app.register_blueprint(user_bp)
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(products_bp)
 
     return app
---
+++ app/routes/auth.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, jsonify, request, current_app
+from app.models.user import LoginPayload
+from pydantic import ValidationError
+import jwt
+from datetime import datetime, timedelta, timezone
+
+auth_bp = Blueprint("auth", __name__)
+
+
+@auth_bp.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "Bem-vindo à API Flask Modularizada!"})
+
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    try:
+        raw_data = request.get_json()
+        user_data = LoginPayload(**raw_data)
+    except ValidationError as e:
+        return jsonify({"message": e.errors()}), 400
+    except Exception:
+        return jsonify({"message": "Erro ao processar dados"}), 500
+
+    # Lógica de validação (Simulada - idealmente buscaria do banco de usuários)
+    if user_data.username == "admin" and user_data.password == "1234":
+        payload = {
+            "user_id": user_data.username,
+            "exp": datetime.now(tz=timezone.utc) + timedelta(hours=1),
+        }
+
+        token = jwt.encode(payload, current_app.config["SECRET_KEY"], algorithm="HS256")
+        return jsonify({"access_token": token}), 200
+    else:
+        return jsonify({"message": "Credenciais inválidas"}), 401
